1e197a0f19a75ec1016862e1b382bdfd668827c4
konstantin0s <constantintofan85@gmail.com>
2018-01-22 20:35:24 +0100

    Update models schema

---
+++ models/students.js
@@ -0,0 +1,18 @@
+// models/classroom.js
+const mongoose = require('../config/database')
+const { Schema } = mongoose
+
+const studentSchema = new Schema({
+
+ classroomId: { type: Schema.Types.ObjectId, ref: 'classrooms' },
+ fullName: { type: String, required: true },
+ photo: { type: String },
+ evaluationDay: { type: [] },
+ evaluationDate: { type: Date, default: Date.now },
+ color: { type: String, default: 'orange' },
+ remark: { type: String, default: null },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('students', studentSchema)
